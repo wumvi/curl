16ddce221e58876bdab922ad117225e16b3546d2
VladKopalov <vladkop@mail.ru>
2020-10-14 01:30:54 +0300

    fix head pipe

---
+++ src/Pipe/HeadMethodPipe.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Wumvi\Curl\Pipe;
+
+class HeadMethodPipe extends Pipe
+{
+    /**
+     * @param resource $curl
+     *
+     * @return $this|Pipe
+     */
+    public function apply($curl): Pipe
+    {
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'HEAD');
+
+        return $this;
+    }
+
+    /**
+     * @param resource $curl
+     */
+    public function remove($curl): void
+    {
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+    }
+}
